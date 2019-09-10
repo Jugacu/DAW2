102b7f2d24a15772df3e7881c3ae458205583f53
Julián Sánchez <julian@jugacu.es>
2019-09-10 19:55:06 +0200

    Server test file

---
+++ Servidor/Test/test.php
@@ -0,0 +1,12 @@
+<html lang="es-ES">
+    <head>
+        <title>xd</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>OwU</h1>
+        <?php
+            echo 'HOLA JUAN'
+        ?>
+    </body>
+</html>
