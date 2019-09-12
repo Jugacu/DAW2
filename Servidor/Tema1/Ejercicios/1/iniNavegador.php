d4ff7f0f967b891bc938b6499d0396734be0b284
Julian <julian@jugacu.es>
2019-09-12 23:30:09 +0200

    Fix

--- Servidor/Tema1/Ejercicios/1/iniNavegador.php
+++ Servidor/Tema1/Ejercicios/1/iniNavegador.php
@@ -1,1 +1,23 @@
 <?php
+// Ejercicio 1
+echo "<b>Tu user-agent es</b>: " . $_SERVER ['HTTP_USER_AGENT'];
+
+// Ejercicio 3
+$var1 = 5;
+$var2 = 10;
+?>
+
+<hr>
+
+<?php
+echo "El valor de var1 es {$var1} <br>";
+echo "El valor de var2 es {$var2} <br>";
+echo "{$var1} + {$var2} son " . ($var1 + $var2) . " <br>";
+echo "{$var1} * {$var2} son " . ($var1 * $var2) . " <br>";
+echo "{$var1} / {$var2} son " . floor($var1 / $var2) . " <br>";
+echo "{$var1} / {$var2} son " . ($var1 / $var2) . " <br>";
+echo "{$var1} % {$var2} son " . ($var1 % $var2) . " <br>";
+?>
+<?php
+// Ejercicio 2
+phpinfo();
