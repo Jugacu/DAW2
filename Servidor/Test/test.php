5673dd003bdd2e97ba241297261891b040c235cb
Julián Sánchez <julian@jugacu.es>
2019-09-12 20:10:37 +0200

    Mas basura

--- Servidor/Test/test.php
+++ Servidor/Test/test.php
@@ -1,3 +1,11 @@
+<?php
+    $var = 'Juan';
+
+    function println($str): void
+    {
+        echo '<br>'.$str;
+    }
+?>
 <html lang="es-ES">
     <head>
         <title>xd</title>
@@ -6,7 +14,39 @@
     <body>
         <h1>OwU</h1>
         <?php
-            echo 'HOLA JUAN'
+            // owo owo owo owo owo owo owo
+            # owo owo owo owo owo owo owo
+            /*
+             * owo owo owo owo owo owo owo owo owo
+             * owo owo owo owo owo owo owo owo owo
+             */
+        ?>
+        <?='Hola Ju$an'?>
+        <?php
+            println("Hola {$var}jo");
+            println('Hola Juan');
+            println(27);
+            println('$var= '.$var);
+
+            $foo = 'owo';
+            $bar = &$foo;
+
+            $bar = "<br>Mi nombre es $bar";
+            echo $bar;
+            echo $foo;
+        ?>
+        <h1>Tests</h1>
+        <?php
+            $test = null;
+            println(var_dump($uwus));
+            println(var_dump($test));
+            echo $test ? 'true' : 'false';
+
+            $foo = isset($awa);
+            println(var_dump($foo));
+
+            $foo = isset($foo);
+            println(var_dump($foo));
         ?>
     </body>
 </html>
