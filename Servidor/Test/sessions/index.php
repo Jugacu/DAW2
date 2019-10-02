a36960a2fc50c3192de7e670e327c704d2d18ea0
Julián Sánchez <julian@jugacu.es>
2019-10-02 21:54:47 +0200

    sessions tests

---
+++ Servidor/Test/sessions/index.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    $_SESSION['OWO'] = 'AWA';
+?>
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+    <title></title>
+</head>
+<body>
+    <a href="uwu.php">UWUUUUU</a>
+</body>
+</html>
---
+++ Servidor/Test/sessions/uwu.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+    <title></title>
+</head>
+<body>
+    <p>
+        $_SESSION['OWO'] => <?=$_SESSION['OWO']?>
+    </p>
+</body>
+</html>
