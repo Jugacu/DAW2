84965409040cc7615bd77e70532ac733008f0731
Julián Sánchez <julian@jugacu.es>
2019-10-25 21:52:32 +0200

    pdo changes

--- Servidor/Test/db/index.php
+++ Servidor/Test/db/index.php
@@ -1,8 +1,9 @@
 <?php
 
 try {
-    $db = new PDO("sqlsrv:Server=localhost;Database=testdb", "root", "rootroot");
+    $db = new PDO("sqlsrv:Server=localhost;Database=testdb"/*, "root", "rootroot"*/);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Failed to get DB handle: " . $e->getMessage() . "\n";
-    exit;
+    die();
 }
