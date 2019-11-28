dbbee987027fb03c5922710c76106142eda12c7c
Julián Sánchez <julian@jugacu.es>
2019-11-28 21:45:56 +0100

    Plastica needs fixes

---
+++ Plastica/PO0505/js/script.js
@@ -0,0 +1,33 @@
+let shouldReset = false;
+
+setInterval(() => {
+    const inputs = document.querySelectorAll('input[name=slide_switch]');
+
+    let nextShouldUpdate = false;
+
+    if (shouldReset) {
+        inputs[0].checked = true;
+    }
+
+    inputs.forEach((input, index) => {
+        // Skip this and make the next checked
+        if (input.checked) {
+            nextShouldUpdate = true;
+            return
+        }
+
+        // Checks it if it needs update
+        if (nextShouldUpdate) {
+            input.checked = true;
+            nextShouldUpdate = false;
+        }
+
+        // If its the last of the array and it needs update it should return to the beginning
+        if (index === inputs.length - 1) {
+            shouldReset = true;
+        }
+
+    })
+
+
+}, 1000);
