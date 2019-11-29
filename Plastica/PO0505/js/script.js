3793fd688db19abf1c8710d8192badc05a54bcf8
Julian <julian@jugacu.es>
2019-11-29 01:11:23 +0100

    PO0505 auto slider

--- Plastica/PO0505/js/script.js
+++ Plastica/PO0505/js/script.js
@@ -1,30 +1,42 @@
-let shouldReset = false;
-let nextShouldUpdate = false;
+const inputs = document.querySelectorAll('input[name=slide_switch]');
+let interval;
 
-setInterval(() => {
-    const inputs = document.querySelectorAll('input[name=slide_switch]');
+// IIFE function
+const resetInterval = (
+    function init() {
+        clearInterval(interval);
 
-    if (shouldReset) {
-        inputs[0].checked = true;
-        shouldReset = false;
-    } else {
-        inputs.forEach((input, index) => {
-            // Skip this and make the next checked
-            if (input.checked && !nextShouldUpdate) {
-                nextShouldUpdate = true;
-                return;
-            }
+        let nextShouldUpdate = false;
 
-            // Checks it if it needs update
-            if (nextShouldUpdate) {
-                input.checked = true;
-                nextShouldUpdate = false;
-            }
+        interval = setInterval(() => {
+            inputs.forEach((input, index) => {
+                // If its the last of the array and its checked it should reset
+                if (index === inputs.length - 1 && input.checked) {
+                    inputs[0].checked = true;
+                    nextShouldUpdate = false;
+                    return;
+                }
 
-            // If its the last of the array and its checked it should reset
-            if (index === inputs.length - 1 && input.checked) {
-                shouldReset = true;
-            }
-        })
+                // Skip this and make the next checked
+                if (input.checked && !nextShouldUpdate) {
+                    nextShouldUpdate = true;
+                    return;
+                }
+
+                // Checks it if it needs update
+                if (nextShouldUpdate) {
+                    input.checked = true;
+                    nextShouldUpdate = false;
+                }
+            })
+
+        }, 5000);
+
+        return init;
     }
-}, 5000);
+)();
+
+// Resets after every click
+inputs.forEach((input) => {
+    input.addEventListener('click', () => resetInterval());
+});
