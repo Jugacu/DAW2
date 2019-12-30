839161837b93f61f25c2d4a7e8162d0e70a44bde
Julián Sánchez <julian@jugacu.es>
2019-12-30 23:44:47 +0100

    menu

---
+++ Plastica/PP0511/bts/assets/js/main.js
@@ -0,0 +1,21 @@
+const recursiveMenu = (el, i = 0) => {
+    if (i > 10) return;
+
+    const children = Array.from(el.childNodes);
+
+    children.forEach((c) => {
+        if (c.nodeType === 1) {
+            if (c.tagName === 'SPAN') {
+                c.addEventListener('click', onclick);
+            }
+            recursiveMenu(c, ++i);
+        }
+    });
+};
+
+function onclick() {
+    window.location.href = `./pages/${this.firstChild.nodeValue.trim()}`
+}
+
+const menu = document.querySelector('#menu');
+recursiveMenu(menu);
