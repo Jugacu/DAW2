52f98765a3d9588730e0bfc906b2c7997e205d75
Julián Sánchez <julian@jugacu.es>
2020-02-13 21:41:48 +0100

    PO0802

--- Servidor/Symfony/src/Controller/HomeController.php
+++ Servidor/Symfony/src/Controller/HomeController.php
@@ -4,20 +4,29 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $user = new User();
         $user->setName('');
+
+        throw new NotFoundHttpException('No eeeeeeeeeeeee');
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+
+
 }
